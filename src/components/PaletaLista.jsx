b251bb16aa213ce01a923734165df7c209ad995c
Raphaelluisg <raphaelluisg@gmail.com>
2022-09-13 12:04:45 -0300

    first commit

---
+++ src/components/PaletaLista.jsx
@@ -0,0 +1,19 @@
+import './PaletaLista.css';
+
+function PaletaLista() {
+    return <div className="PaletaLista">
+        <div className="PaletaListaItem">
+            <div>
+                <div className="PaletaListaItem__titulo">Doce de Leite</div>
+                <div className="PaletaListaItem__preco">R$ 10,00</div>
+                <div className="PaletaListaItem__descricao">Paleta de doce de leite</div>
+                <div className="PaletaListaItem__acoes Acoes">
+                    <button className="Acoes__adicionar Acoes__adicionar--preencher">Add</button>
+                </div>
+            </div>
+            <img className="PaletaListaItem__foto" src={require("../assets/image/images/doce-de-leite-com-doce-de-leite.png")} alt="Paleta de doce de leite" />
+        </div>
+    </div>;
+}
+
+export default PaletaLista;
